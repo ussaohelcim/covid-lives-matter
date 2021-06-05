a876faf4a50b16512a8b4553ef118630336565d2
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2021-06-05 19:16:08 -0300

    arrumado erro cena dos creditos

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ import SplashScreen from './splashscreen.js';
 import Menu from './menu.js';
 import Fases from './fases.js';
 import Instrucoes from './instrucoes.js';
+import Creditos from './creditos.js'
 
 let splash = new SplashScreen();
 let menu = new Menu();
 let fases = new Fases();
 let instrucoes = new Instrucoes();
+let creditos = new Cr
 
 let config = {
     type: Phaser.AUTO,
